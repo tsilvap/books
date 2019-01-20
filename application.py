6cd4e4938001b2a966658d7409ce57395c171e0f
Thiago da Silva <thiagodasilva@protonmail.com>
2019-01-19 23:33:14 -0200

    Start implementing registration

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, session
+from flask import Flask, render_template, request, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -20,17 +20,56 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+# Helper functions
+def valid_registration(username, email):
+    """Return whether the username or the email are not already in the database."""
+    if (
+        db.execute(
+            "SELECT * FROM users WHERE username=:username OR email=:email",
+            {"username": username, "email": email},
+        ).fetchone()
+        is not None
+    ):
+        return False
 
+    return True
+
+
+def register_user(username, email, password):
+    """Register a new user into the database."""
+    db.execute(
+        "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)",
+        {"username": username, "email": email, "password": password},
+    )
+    db.commit()
+
+
+# Routes
 @app.route("/")
 def index():
+    """Index page."""
     return render_template("index.html.j2")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    """Register page."""
+    if request.method == "POST":
+        username, email, password = (
+            request.form["username"],
+            request.form["email"],
+            request.form["password"],
+        )
+        if valid_registration(username, email):
+            register_user(username, email, password)
+            return render_template("register.html.j2")
+        else:
+            pass  # FIXME: Implement this.
+
     return render_template("register.html.j2")
 
 
 @app.route("/login")
 def login():
+    """Login page."""
     return render_template("login.html.j2")
