213d094b6d42de8a08f0a00a265a4318efe7f072
Thiago da Silva <thiagodasilva@protonmail.com>
2019-01-20 15:10:21 -0200

    Improve register implementation

--- application.py
+++ application.py
@@ -1,6 +1,14 @@
 import os
 
-from flask import Flask, render_template, request, session
+from flask import (
+    Flask,
+    flash,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -21,15 +29,41 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 # Helper functions
-def valid_registration(username, email):
-    """Return whether the username or the email are not already in the database."""
-    if (
-        db.execute(
-            "SELECT * FROM users WHERE username=:username OR email=:email",
-            {"username": username, "email": email},
-        ).fetchone()
-        is not None
-    ):
+def username_already_in_use(username):
+    """Return whether username is already in use."""
+    username_in_database = db.execute(
+        "SELECT username FROM users where username=:username",
+        {"username": username},
+    ).fetchone()
+    if username_in_database:
+        return True
+
+    return False
+
+
+def email_already_in_use(email):
+    """Return whether email is already in use."""
+    email_in_database = db.execute(
+        "SELECT email FROM users where email=:email", {"email": email}
+    ).fetchone()
+    if email_in_database:
+        return True
+
+    return False
+
+
+def valid_registration(username, email, password):
+    """Return whether the username, email, and password are valid.
+
+    Namely, it returns True if all of the parameters are non-empty, and
+    if neither the username or email have already been registered
+    before.
+    """
+    # Ensure all parameters are non-empty strings
+    if len(username) * len(email) * len(password) == 0:
+        return False
+
+    if username_already_in_use(username) or email_already_in_use(email):
         return False
 
     return True
@@ -38,7 +72,8 @@ def valid_registration(username, email):
 def register_user(username, email, password):
     """Register a new user into the database."""
     db.execute(
-        "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)",
+        "INSERT INTO users (username, email, password)"
+        "VALUES (:username, :email, :password)",
         {"username": username, "email": email, "password": password},
     )
     db.commit()
@@ -56,15 +91,23 @@ def register():
     """Register page."""
     if request.method == "POST":
         username, email, password = (
-            request.form["username"],
-            request.form["email"],
-            request.form["password"],
+            str(request.form["username"]),
+            str(request.form["email"]),
+            str(request.form["password"]),
         )
-        if valid_registration(username, email):
+        if valid_registration(username, email, password):
             register_user(username, email, password)
-            return render_template("register.html.j2")
+            flash("Successfully registered! You may now log in.", "success")
+            return redirect(url_for("login"))
         else:
-            pass  # FIXME: Implement this.
+            if username_already_in_use(username):
+                flash(
+                    "Username already in use, please pick another.", "danger"
+                )
+            if email_already_in_use(email):
+                flash("Email already in use, please pick another.", "danger")
+
+            return redirect(url_for("register"))
 
     return render_template("register.html.j2")
 
